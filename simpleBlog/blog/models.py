7b2dc9fe8cda9afc417a15f990ec481f6825cdee
Cyrill <lichunyang@outlook.com>
2017-08-05 15:11:26 +0800

    cc

--- simpleBlog/blog/models.py
+++ simpleBlog/blog/models.py
@@ -3,12 +3,41 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
+class Tag(models.Model):
+
+    """标签"""
+    name = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+
+    """分类目录"""
+    name = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.name
+
+
 class Post(models.Model):
 
+    """文章"""
+
+    # 作者
     author = models.ForeignKey(User)
+    # 标题
     title = models.CharField(max_length=200)
+    # 正文
     text = models.TextField()
-    create_date = models.DateTimeField(default=timezone.now())
+    # 标签
+    tags = models.ManyToManyField(Tag)
+    # 点击量
+    click = models.IntegerField(default=0)
+    # 创建时间
+    created_date = models.DateTimeField(default=timezone.now)
+    # 发布时间
     published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
--- simpleBlog/simpleBlog/urls.py
+++ simpleBlog/simpleBlog/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
+from blog import views as blog_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', blog_views.post_list, name='blog_post_list'),
+    url(r'^post/(?P<pk>[0-9]+)/$', blog_views.post_detail, name='blog_post_detail'),
 ]
