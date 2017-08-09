f1f44f906449c2171b55d8f5a375139c7595a549
Cyrill <lichunyang@outlook.com>
2017-08-09 16:56:14 +0800

    cc

--- simpleBlog/blog/views.py
+++ simpleBlog/blog/views.py
@@ -5,21 +5,38 @@ from django.shortcuts import get_object_or_404
 from django.shortcuts import redirect
 from django.core.urlresolvers import reverse
 from django.http import HttpRequest
+from django.core.paginator import PageNotAnInteger, EmptyPage, Paginator
 
 
 def post_list(request):
     """所有已发布文章"""
 
     postsAll = Post.objects.filter(published_date__isnull=False).order_by('-published_date')
-    posts = postsAll
-    for post in posts:
-        post.detail = post.text[:100] + '【更多】'
+    paginator = Paginator(postsAll, 2)  # Show 10 contacts per page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post_list.html', {'posts' : posts})
 
 
 def post_draft_list(request):
     postsAll = Post.objects.filter(published_date__isnull=True).order_by('-created_date')
-    posts = postsAll
+    paginator = Paginator(postsAll, 2)  # Show 10 contacts per page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
 
 
